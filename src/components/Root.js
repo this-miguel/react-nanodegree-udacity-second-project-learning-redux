dfa2314a07b99ab2d0a581d6cff9d1c3b7af82e1
Miguel Salas <miguel@kundigo.pro>
2017-09-18 02:51:00 -0400

    feat: modifies the Root component to connect it to the redux state,  and show routes that match with categories paths. In each Route  renders a PostList component that in turn renders the post titles.

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,22 +1,85 @@
-import React from 'react'
+import React, { Component }  from 'react'
 import PropTypes from 'prop-types'
 import { Provider } from 'react-redux'
 import { Router, Route  } from 'react-router'
 import createHistory from 'history/createBrowserHistory'
-import App from './App'
+import CategoryList from './CategoryList'
+import PostListConnected from './PostList'
+import {connect} from 'react-redux'
 
-const history = createHistory()
+import {
+  asyncGetCategories
+} from '../actions/AsychActions'
 
-const Root = ({ store }) => (
-  <Provider store={store}>
-    <Router history={history}>
-      <Route  exact path="/" component={App} />
-    </Router>
-  </Provider>
-)
+const history = createHistory();
 
-Root.propTypes = {
-  store: PropTypes.object.isRequired
+class Root extends Component {
+
+  componentWillMount() {
+    const {getCategories } = this.props;
+    getCategories();
+  }
+
+  render(){
+    const { store, categoriesIds } = this.props;
+
+    return(
+      <Provider store={store}>
+        <Router history={history}>
+          <div className="app">
+            <CategoryList/>
+            {
+              categoriesIds.map(categoryId => (
+                <Route
+                  key={`/${categoryId}-route`}
+                  exact path={`/${categoryId}`}
+                  render={ () => (
+                    <PostListConnected
+                      key={`/${categoryId}-post-list`}
+                      category={categoryId}
+                    />
+                  )}
+                />
+              ))
+            }
+            <Route
+              exact path='/'
+              render={ () => (
+                <PostListConnected
+                  category='all'
+                />
+              )}
+            />
+
+          </div>
+        </Router>
+      </Provider>
+    )
+  }
 }
 
-export default Root
+function mapDispatchToProps(dispatch){
+  return {
+    getCategories: asyncGetCategories(dispatch),
+  }
+}
+
+function mapStateToProps(state){
+  return {
+    categories: state.categories,
+    categoriesIds: state.categoriesIds,
+  }
+}
+
+const RootConnected = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Root);
+
+Root.propTypes = {
+  store: PropTypes.object.isRequired,
+  categories: PropTypes.object.isRequired,
+  categoriesIds: PropTypes.array.isRequired,
+};
+
+export default RootConnected
