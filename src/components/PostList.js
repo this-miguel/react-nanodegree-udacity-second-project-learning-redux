ba98fbd8c7852abc19a6510d757a477ff65cb360
Miguel Salas <miguel@kundigo.pro>
2017-09-20 03:39:55 -0400

    feat: Links to the Post using the title in the post list.

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import {
@@ -43,7 +44,7 @@ class PostList extends Component {
         {
           postsToRender.map(post => (
             <li key={`${post.id}-${category}`}>
-              <div>title: {post.title} </div>
+              <Link to={`${post.category}/${post.id}`}> {post.title} </Link>
             </li>
           ))
         }
