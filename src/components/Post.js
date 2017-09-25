d27c3a75fc27ee24af273f60016968765cb98c7e
Miguel Salas <miguel@kundigo.pro>
2017-09-24 23:29:42 -0400

    feat: shows post info in a panel inside atable inside the Post component.

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux'
+import {asyncVoteForAPost } from '../actions/AsychActions'
 
 class Post extends Component {
   render(){
-    const  {posts, postId } = this.props;
+    const  {posts, postId, upvotePost, downvotePost } = this.props;
     const  post = posts[postId];
+    const headers =  [ 'Comments', 'Score', 'Vote', 'Date' ];
     return (
       <div>
         <h3>{post.title}</h3>
         <p>{post.body}</p>
-        <p> by {post.author}</p>
+        <p> by {post.author} </p>
+
+        <div className="panel panel-default">
+          <div className="panel-heading">Post Info</div>
+            <table className="table">
+            <thead>
+            <tr>
+            {
+              headers.map((header)=> (<th key={header}>{header}</th>))
+            }
+          </tr>
+          </thead>
+            <tbody>
+            <tr>
+              <td>
+                {post.comments ? post.comments.length : '...' }
+              </td>
+              <td>
+                {post.voteScore}
+              </td>
+              <td>
+                <button style={{display: 'inline-block'}} onClick={upvotePost}>
+                  <span className='glyphicon glyphicon-chevron-up' alt='upvote'></span>
+                </button>
+                <button onClick={downvotePost}>
+                  <span className='glyphicon glyphicon-chevron-down' alt='downvote'></span>
+                </button>
+              </td>
+              <td>
+                {new Date(post.timestamp).toDateString()}
+              </td>
+
+            </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
       )
   }
@@ -21,8 +58,16 @@ function mapStateToProps(state){
   }
 }
 
+function mapDispatchToProps(dispatch, OwnProps){
+  const {  postId }      = OwnProps;
+  return {
+    upvotePost:asyncVoteForAPost(dispatch)(postId, 'upVote'),
+    downvotePost:asyncVoteForAPost(dispatch)(postId, 'downVote'),
+  }
+}
 const PostConnected =  connect(
-  mapStateToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(Post);
 
 export default PostConnected
