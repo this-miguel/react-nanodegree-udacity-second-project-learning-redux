f4a07f612fdb05098145c51dc5bec73e633f05f4
Miguel Salas <miguel@kundigo.pro>
2017-09-27 04:03:27 -0400

    fix: I forgot to add the new Coment Modal component.

---
+++ src/components/ComentModal.js
@@ -0,0 +1,64 @@
+import React, { Component }  from 'react'
+import PropTypes from 'prop-types'
+import Modal from 'react-modal'
+import createHistory from 'history/createBrowserHistory'
+import {clearAndCloseModal} from '../actions/modalActions';
+import modalStyles from './common/modalStyles'
+import {connect} from 'react-redux'
+
+import {
+  asyncGetCategories
+} from '../actions/AsychActions'
+
+class CommentModalDisconnected extends Component {
+
+  componentWillMount() {
+
+  }
+  render(){
+
+    const {
+      isOpen,
+      closeModal,
+      ...others
+    } = this.props;
+    return(
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={closeModal}
+        className='react-modal'
+        contentLabel='Modal'
+        closeTimeoutMS={275}
+        style={modalStyles}
+
+
+      >
+
+            HOLLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+
+      </Modal>
+    )
+  }
+}
+
+function mapStateToProps (state, ownProps) {
+  return {
+    isOpen: state.activeModal === ownProps.modalAction
+  };
+}
+
+
+function mapDispatchToProps (dispatch, ownProps) {
+  return{
+    closeModal: function () {
+      dispatch(clearAndCloseModal())
+    }
+  }
+}
+
+const CommentModal = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CommentModalDisconnected);
+
+export default CommentModal
